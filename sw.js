4f7399a7cff2e06047efe67beead9fd0a2490aa7
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-14 06:14:40 +0000

    Add one-tap reverse journey control

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mtr-smart-progress-view-v7';
+const CACHE='mtr-smart-reverse-direction-v8';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./mtr-network.js','./manifest.json','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
