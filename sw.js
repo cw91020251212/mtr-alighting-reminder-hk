730152087bf6c206b38c9be5d348901172edfcb0
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-14 11:17:44 +0000

    Track GPS history with labeled inferred gaps

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mtr-smart-train-screensaver-v11';
+const CACHE='mtr-smart-truthful-location-v12';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./mtr-network.js','./manifest.json','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
